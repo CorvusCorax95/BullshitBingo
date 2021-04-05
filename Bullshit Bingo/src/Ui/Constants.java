2e441ede03f27309e96d213777a106e4439514f9
Lisa Stillenmunkes <flohlisa36@gmail.com>
2021-04-05 12:50:46 +0200

    still planing

---
+++ Bullshit Bingo/src/Ui/Constants.java
@@ -0,0 +1,10 @@
+package Ui;
+
+public class Constants {
+
+    public static final int LINES = 4;
+    public static final int COLUMNS = 4;
+    public static final int MAIN_WIDTH = 800;
+
+
+}
--- Bullshit Bingo/src/Ui/Ui.java
+++ Bullshit Bingo/src/Ui/Ui.java
@@ -1,4 +1,22 @@
 package Ui;
 
+import javax.swing.*;
+
 public class Ui {
+    private JPanel rootPane;
+    //private final JFrame mainFrame;
+
+    //board is for actual game, while characters is used for choosing a character.
+   // private JLabel[][] board = null;
+    //private JLabel[] fields = null;
+
+    public Ui() {
+       // mainFrame = new JFrame();
+       // mainFrame.setTitle("Bullshit Bingo!");
+
+
+
+
+    }
+
 }
