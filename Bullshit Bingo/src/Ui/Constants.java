60c486cbd990be875bc346eab6b58e5fb1a75461
Lisa Stillenmunkes <flohlisa36@gmail.com>
2021-04-05 12:58:14 +0200

    pushing

--- Bullshit Bingo/src/Ui/Constants.java
+++ Bullshit Bingo/src/Ui/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final int LINES = 4;
     public static final int COLUMNS = 4;
     public static final int MAIN_WIDTH = 800;
+    public static final int MAIN_HEIGHT = 600;
+
 
 
 }
--- Bullshit Bingo/src/Ui/Ui.java
+++ Bullshit Bingo/src/Ui/Ui.java
@@ -4,18 +4,19 @@ import javax.swing.*;
 
 public class Ui {
     private JPanel rootPane;
-    //private final JFrame mainFrame;
+    private final JFrame mainFrame;
 
-    //board is for actual game, while characters is used for choosing a character.
-   // private JLabel[][] board = null;
-    //private JLabel[] fields = null;
+    private JLabel[][] board = null;
 
     public Ui() {
-       // mainFrame = new JFrame();
-       // mainFrame.setTitle("Bullshit Bingo!");
+        mainFrame = new JFrame();
+        mainFrame.setTitle("Bullshit Bingo!");
 
+        mainFrame.setSize(Constants.MAIN_WIDTH, Constants.MAIN_HEIGHT);
+        mainFrame.setLayout(null);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
+        board = new JLabel[Constants.COLUMNS][Constants.LINES];
 
     }
 
