2ec8fb5fe02b6b3e445bfa01edea8c5178434763
Lisa Stillenmunkes <flohlisa36@gmail.com>
2021-04-05 13:11:04 +0200

    it does things

--- Bullshit Bingo/src/Main.java
+++ Bullshit Bingo/src/Main.java
@@ -1,6 +1,12 @@
+import Ui.Ui;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Ui ui = new Ui();
+
+        ui.initBoard();
 
     }
 
--- Bullshit Bingo/src/Ui/Ui.java
+++ Bullshit Bingo/src/Ui/Ui.java
@@ -1,8 +1,11 @@
 package Ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Ui {
+public class Ui implements MouseListener {
     private JPanel rootPane;
     private final JFrame mainFrame;
 
@@ -20,4 +23,51 @@ public class Ui {
 
     }
 
+    public void initBoard() {
+
+        this.mainFrame.setVisible(false);
+        this.mainFrame.getContentPane().removeAll();
+        for (int i = 0; i < Constants.LINES; i++) {
+            for (int j = 0; j < Constants.COLUMNS; j++) {
+                board[i][j] = new JLabel();
+                board[i][j].setName("board" + i + j);
+                board[i][j].setBounds(j * 100 + 50, i * 100, 100, 100);
+                board[i][j].setText("place holder");
+                board[i][j].addMouseListener(this);
+                mainFrame.getContentPane().add(board[i][j]);
+
+            }
+        }
+        mainFrame.getContentPane().revalidate();
+        mainFrame.getContentPane().repaint();
+        mainFrame.setVisible(true);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        e.getComponent().setBackground(Color.GREEN);
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
